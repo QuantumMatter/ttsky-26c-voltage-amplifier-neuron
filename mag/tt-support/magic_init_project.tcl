57b2c2db48517b7d980986acb0f203790239cfc9
QuantumMatter <kopala.david@gmail.com>
2026-09-06 20:08:32 -0400

    Another fix?

--- mag/tt-support/magic_init_project.tcl
+++ mag/tt-support/magic_init_project.tcl
@@ -9,7 +9,7 @@
 # Change the settings below to match your design:
 # ------------------------------------------------
 set TOP_LEVEL_CELL     tt_um_voltage_amplifier_neuron
-set TEMPLATE_FILE      tt_analog_1x2.def
+set TEMPLATE_FILE      tt-support/tt_analog_1x2.def
 set POWER_STRIPE_WIDTH 2um                 ;# The minimum width is 1.2um
 
 # Power stripes: NET name, x position. You can add additional power stripes for each net, as needed.
@@ -48,7 +48,7 @@ foreach {name x} $POWER_STRIPES {
 # Save the layout and export GDS/LEF
 # ----------------------------------
 save ${TOP_LEVEL_CELL}.mag
-file mkdir ../../gds
-gds write ../../gds/${TOP_LEVEL_CELL}.gds
-file mkdir ../../lef
-lef write ../../lef/${TOP_LEVEL_CELL}.lef -hide -pinonly
+file mkdir ../gds
+gds write ../gds/${TOP_LEVEL_CELL}.gds
+file mkdir ../lef
+lef write ../lef/${TOP_LEVEL_CELL}.lef -hide -pinonly
